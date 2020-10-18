3e0575c41bdef38250971dac78cb0afbcce3f3a8
Aaraj Yousuf <aarajyousufdiu@gmail.com>
2020-10-18 12:33:37 +0600

    set up completed

--- app/src/main/java/com/example/smarttablayout/MainActivity.kt
+++ app/src/main/java/com/example/smarttablayout/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.smarttablayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pages = FragmentPagerItems(this)
+        pages.add(FragmentPagerItem.of("Tab1", Tab1Fragment::class.java))
+        pages.add(FragmentPagerItem.of("Tab2", Tab2Fragment::class.java ))
+        pages.add(FragmentPagerItem.of("Tab3", Tab3Fragment::class.java ))
+        pages.add(FragmentPagerItem.of("Tab4", Tab4Fragment::class.java ))
+        pages.add(FragmentPagerItem.of("Tab5", Tab5Fragment::class.java ))
+
+        val adapter = FragmentPagerItemAdapter(supportFragmentManager, pages)
+
+        viewpager.adapter = adapter
+        tabLayout.setViewPager(viewpager)
     }
 }
